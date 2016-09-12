b16d01e91933ece0e42daf745998e5487c98edd5
rsinghania <rsinghania@connecture.com>
2016-09-12 16:35:05 +0530

    HelloWorld Microservice

---
+++ src/main/java/main/model/HelloWorld.java
@@ -0,0 +1,16 @@
+package main.model;
+
+public class HelloWorld
+{
+  private String hello;
+
+  public String getHello()
+  {
+    return hello;
+  }
+
+  public void setHello(String name)
+  {
+    hello = "Hello " + name;
+  }
+}
